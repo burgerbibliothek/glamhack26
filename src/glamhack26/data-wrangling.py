1785a48b6d550c6d62a1928d0635feb8fc7df088
Giò Diani <giodi@users.noreply.github.com>
2026-07-29 08:57:10 +0000

    cleanup

--- src/glamhack26/data-wrangling.py
+++ src/glamhack26/data-wrangling.py
@@ -2,7 +2,7 @@ import polars as pl
 import pathlib
 
 # Export list containing only IDs of descriptors of the type "Orte"
-df = pl.read_csv("./export_ve_desk.csv", separator=";")
+df = pl.read_parquet("./export_ve_desk.parquet")
 get_places_ids = df.sql("SELECT vrzng_enht_id, dskrp_id, ve_gsft_obj_kurz_nm, gsft_obj_bzhng_rolle_nm, thsrs_nm FROM self WHERE thsrs_nm = 'Orte' ORDER BY dskrp_id")
 get_places_ids.write_parquet("./export_ve_desk_orte.parquet")
 print('Saved export_desk_orte.parquet')
