61c311f48b947bf9740d497263c7cd2ad97ae030
Giò Diani <giodi@users.noreply.github.com>
2026-07-29 11:50:52 +0000

    path correction

--- src/glamhack26/data-wrangling.py
+++ src/glamhack26/data-wrangling.py
@@ -6,5 +6,5 @@ p = Path(__file__).parent
 # Export list containing only IDs of descriptors of the type "Orte"
 df = pl.read_parquet(p / "export_ve_desk.parquet")
 get_places_ids = df.sql("SELECT vrzng_enht_id, dskrp_id, ve_gsft_obj_kurz_nm, gsft_obj_bzhng_rolle_nm, thsrs_nm FROM self WHERE thsrs_nm = 'Orte' ORDER BY dskrp_id")
-get_places_ids.write_parquet("./export_ve_desk_orte.parquet")
+get_places_ids.write_parquet(p / "export_ve_desk_orte.parquet")
 print('Saved export_desk_orte.parquet')
